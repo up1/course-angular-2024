1e7ad371a303764bb4f3c99ba1716cdc54ee62a0
up1 <somkiat.p@gmail.com>
2024-03-18 15:40:39 +0700

    Working with angular form

--- demo-auth/src/app/app.module.ts
+++ demo-auth/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- demo-auth/src/app/register/register.component.ts
+++ demo-auth/src/app/register/register.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrl: "./register.component.css",
 })
 export class RegisterComponent {
+  form: any = {
+    username: null,
+    email: null,
+    password: null,
+  };
+  onSubmit() {
+    const { username, email, password } = this.form;
 
+    console.log("Username:", username);
+    console.log("Email:", email);
+    console.log("Password:", password);
+  }
 }
