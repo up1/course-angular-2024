769fcac7aaee6cb307c1947b2a8c1744d716f2f8
up1 <somkiat.p@gmail.com>
2024-03-18 16:20:12 +0700

    Refactor to model class

---
+++ demo-auth/src/app/models/user-form.ts
@@ -0,0 +1,11 @@
+export class UserForm {
+  username: string;
+  email: string;
+  password: string;
+
+  constructor() {
+    this.username = "";
+    this.email = "";
+    this.password = "";
+  }
+}
--- demo-auth/src/app/register/register.component.ts
+++ demo-auth/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { UserForm } from "../models/user-form";
 
 @Component({
   selector: "app-register",
@@ -6,11 +7,8 @@ import { Component } from "@angular/core";
   styleUrl: "./register.component.css",
 })
 export class RegisterComponent {
-  form: any = {
-    username: null,
-    email: null,
-    password: null,
-  };
+  form: UserForm = new UserForm();
+
   onSubmit() {
     const { username, email, password } = this.form;
 
